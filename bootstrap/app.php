e7e0e8907ebcb28f6dd1b437a8f014b3ccbf9fac
bunlong <nathbunlong@gmail.com>
2026-08-27 08:20:29 +0700

    Trust Render's proxy and force HTTP URLS

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,6 +19,8 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
 
         $middleware->redirectGuestsTo('/login');
+
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
